152cb4df7d996d82050eb8bb128d7a77e6882fb1
GustavoM7 <gustavosmarquesf@gmail.com>
2019-09-10 19:54:37 -0300

    merge sort

---
+++ merge-sort.hpp
@@ -0,0 +1,39 @@
+#ifndef MERGESORT_HPP
+#define MERGESORT_HPP
+#include <iostream>
+using namespace std;
+
+//complexidade O(nlogn)
+
+void merge(int vetor[], int vetorAux[], int comeco, int meio, int fim){
+    int esq, dir;
+    esq = comeco;
+    dir = meio;
+
+    for(int i=comeco; i < fim; i++){
+        if(esq < meio && (dir >= fim || vetor[esq] < vetor[dir])){
+            vetorAux[i] = vetor[esq];
+            esq++;
+        } else {
+            vetorAux[i] = vetor[dir];
+            dir++;
+        }
+    }
+
+    for(int i = comeco; i < fim; i++){
+        vetor[i] = vetorAux[i];
+    }
+
+}
+
+
+void mergeSort(int vetor[], int vetorAux[], int comeco, int fim){
+    if(fim - comeco < 2) return;
+
+    int meio = (comeco + fim)/2;
+    mergeSort(vetor, vetorAux, comeco, meio);
+    mergeSort(vetor, vetorAux, meio, fim);
+    merge(vetor, vetorAux, comeco, meio, fim);
+}
+
+#endif
--- testes-interface.cpp
+++ testes-interface.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 #include <time.h>
 #include "insertion-sort.hpp"
+#include "merge-sort.hpp"
 
 using namespace std;
+
+void insertionsort_teste();
+void mergeSort_teste();
+
+int main(){
+    int input;
+
+    cout << "Selecione a implementação" << endl;
+    cout << "1: Insertion Sort" << endl;
+    cout << "2: Merge Sort" << endl;
+
+    cin >> input;
+
+    switch(input){
+        case 1: 
+            insertionsort_teste();
+            break;
+        case 2: 
+            mergeSort_teste();
+            break;
+        default:
+            cout << "Valor inválido..." << endl;
+    }
+}
 
 void insertionsort_teste(){
     int * A;
@@ -34,16 +59,33 @@ void insertionsort_teste(){
     printf("Tempo gasto: %g ms \n", t);
 }
 
+void mergeSort_teste(){
+    int n, e;
+    double t;
 
-int main(){
-    int input;
+    cout << "Entre o tamanho do vetor: ";
+    cin >> n;
+    int A[n];
+    int Aux[n];
 
-    cout << "Selecione a implementação" << endl;
-    cout << "1: Insertion Sort" << endl;
-
-    cin >> input;
-
-    if(input == 1){
-        insertionsort_teste();
+    cout << "Entre os elementos do vetor:" << endl;
+    for(int i = 0; i < n; i ++){
+        cin >> A[i];
     }
+
+    cout << "Executando ordenação..." << endl;
+
+    clock_t Ticks[2];
+    Ticks[0] = clock();
+    mergeSort(A, Aux, 0, n);
+    Ticks[1] = clock();
+
+    t = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+
+    cout << "Vetor ordenado:" << endl;
+    for(int i = 0; i < n; i ++){
+        cout << A[i] << endl;
+    }
+
+    printf("Tempo gasto: %g ms \n", t);
 }
